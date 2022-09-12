dd491f9e1160aad203683774bcf3b39230040ea5
Yader Oneall Campbell <yadermejiac123@gmail.com>
2022-09-11 21:07:21 -0600

    feat: Modifying the main API route, locally

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,9 +2,11 @@ class AppConstants {
   static const String APP_NAME = "DBFood";
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "https://mvs.bslmeiyu.com";
+  //URL LOCAL
+  static const String BASE_URL = "http://127.0.0.1:8000";
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
+  static const String DRINKS_URI = "/api/v1/products/drinks"; //Nueva ruta creada en el admin Laravel
   static const String UPLOAD_URL = "/uploads/";
 
   static const String TOKEN = "DBtoken";
