8664d30b94eb762dfef389a4efe6c85247df36ca
Yader Oneall Campbell <57229141+Yader1@users.noreply.github.com>
2022-09-12 14:03:30 -0600

    feat: Reload main page (#4)

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tienda_comida/controllers/popular_product_controller.dart';
 import 'package:flutter_tienda_comida/pages/home/food_page_body.dart';
 import 'package:flutter_tienda_comida/utils/colors.dart';
 import 'package:flutter_tienda_comida/utils/dimensiones.dart';
 import 'package:flutter_tienda_comida/widgets/big_text.dart';
 import 'package:flutter_tienda_comida/widgets/small_text.dart';
+import 'package:get/get.dart';
+
+import '../../controllers/recommended_product_controller.dart';
 
 class MainFoodPage extends StatefulWidget {
   MainFoodPage({Key? key}) : super(key: key);
@@ -12,46 +16,65 @@ class MainFoodPage extends StatefulWidget {
   State<MainFoodPage> createState() => _MainFoodPageState();
 }
 
+Future<void> _loadResource () async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-      children: [
-        //Showing the header
-        Container(
-          child: Container(
-            //Encabezado de la pagina principal
-            margin: EdgeInsets.only(top: Dimenciones.height45, bottom: Dimenciones.height15),
-            padding: EdgeInsets.only(left: Dimenciones.width20, right: Dimenciones.width20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    BigText(text: "Bengladesh", color: AppColors.mainColor,), //Pais
-                    //Hacer un tipo lista del departamento
-                    Row(children: [
-                      SmallText(text: "Narsingdi", color: Colors.black54),
-                      Icon(Icons.arrow_drop_down_circle_rounded)
-                    ],)
-                  ], 
-                ),
-                Center(
-                  child: Container(
+    return RefreshIndicator(
+        child: Column(children: [
+          //Showing the header
+          Container(
+            child: Container(
+              //Encabezado de la pagina principal
+              margin: EdgeInsets.only(
+                  top: Dimenciones.height45, bottom: Dimenciones.height15),
+              padding: EdgeInsets.only(
+                  left: Dimenciones.width20, right: Dimenciones.width20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "Bengladesh",
+                        color: AppColors.mainColor,
+                      ), //Pais
+                      //Hacer un tipo lista del departamento
+                      Row(
+                        children: [
+                          SmallText(text: "Narsingdi", color: Colors.black54),
+                          Icon(Icons.arrow_drop_down_circle_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                      child: Container(
                     width: Dimenciones.height45,
                     height: Dimenciones.height45,
-                    child: Icon(Icons.search, color: Colors.white, size: Dimenciones.iconSize24,),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: Dimenciones.iconSize24,
+                    ),
                     decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(Dimenciones.radius15), color: AppColors.mainColor),
-                  )
-                )
-              ],),
-          ),),
-        //Showing the body
-        Expanded(child: SingleChildScrollView(
-          child: FoodPageBody(),))
-      ]),
-    );
+                        borderRadius:
+                            BorderRadius.circular(Dimenciones.radius15),
+                        color: AppColors.mainColor),
+                  ))
+                ],
+              ),
+            ),
+          ),
+          //Showing the body
+          Expanded(
+              child: SingleChildScrollView(
+            child: FoodPageBody(),
+          ))
+        ]),
+        onRefresh: _loadResource);
   }
 }
